9b1367cde196f05cdbb3a1270ffe19db7be94ba4
Michael Raidel <raidel@induktiv.at>
2017-03-17 13:03:12 +0100

    failure

--- spec/phone_number_spec.rb
+++ spec/phone_number_spec.rb
@@ -2,6 +2,6 @@ require_relative '../phone_number'
 
 describe PhoneNumber do
   it "normalizes" do
-    expect(PhoneNumber.new("+43-699/100").normalized).to eq("43699100")
+    expect(PhoneNumber.new("+43-699/100").normalized).to eq("43699102")
   end
 end
