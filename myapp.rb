1d601ab79c8b635ce3ec592e4241bc3538cda76d
Michael Raidel <raidel@induktiv.at>
2017-03-17 12:31:19 +0100

    initial commit

---
+++ myapp.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require_relative 'phone_number'
+
+get '/' do
+  slim :index
+end
+
+post '/' do
+  @number = params[:number]
+  @normalized = PhoneNumber.new(@number).normalized
+  slim :index
+end
+
+
